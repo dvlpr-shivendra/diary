e7418f1d8fab236ea501ba08f353ab49da0cc920
Shivendra Techster <shivendratechster@gmail.com>
2021-04-25 22:10:16 +0530

    minor ui fixes

--- resources/views/entries/index.blade.php
+++ resources/views/entries/index.blade.php
@@ -10,7 +10,7 @@
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                     <div class="p-6 bg-white border-b border-gray-200">
-                        <p>{{ $entry->created_at->format('Y-m-d') }}</p>
+                        <p>{{ $entry->created_at->format('d M, Y') }}</p>
                         <div class="my-4 italic text-2xl">{{ Str::limit($entry->body, 100) }}</div>
                         <a href="{{ route('entries.show', $entry) }}"
                             class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -6,7 +6,7 @@
                 <!-- Logo -->
                 <div class="flex-shrink-0 flex items-center">
                     <a href="{{ route('entries.index') }}">
-                        <x-application-logo class="block h-10 w-auto fill-current text-gray-600" />
+                        Diary
                     </a>
                 </div>
 
