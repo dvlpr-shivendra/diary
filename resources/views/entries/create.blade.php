55c60b341916a847827fac711d648303402ff70b
Shivendra Techster <shivendratechster@gmail.com>
2021-05-14 02:07:24 +0530

    use trix for creating posts

--- resources/views/entries/create.blade.php
+++ resources/views/entries/create.blade.php
@@ -1,27 +1,36 @@
-<x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Create entry') }}
-        </h2>
-    </x-slot>
+@extends('layouts.app')
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 bg-white border-b border-gray-200">
-                    <form action="{{ route('entries.store') }}" method="post">
-                        @csrf
-                        <label class="block text-gray-700 text-sm font-bold mb-2" for="body">Today's entry</label>
-                        <textarea class="block mb-2 w-full h-80 resize-y border rounded-md" name="body"
-                            id="body"></textarea>
+@section('content')
 
-                        <button
-                            class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
-                            Save
-                        </button>
-                    </form>
-                </div>
+@push('head')
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/trix/1.3.1/trix.min.js"
+        integrity="sha512-2RLMQRNr+D47nbLnsbEqtEmgKy67OSCpWJjJM394czt99xj3jJJJBQ43K7lJpfYAYtvekeyzqfZTx2mqoDh7vg=="
+        crossorigin="anonymous"></script>
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/trix/1.3.1/trix.min.css"
+        integrity="sha512-5m1IeUDKtuFGvfgz32VVD0Jd/ySGX7xdLxhqemTmThxHdgqlgPdupWoSN8ThtUSLpAGBvA8DY2oO7jJCrGdxoA=="
+        crossorigin="anonymous" />
+@endpush
+
+
+<div class="py-12">
+    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+        <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+            <div class="p-6 bg-white border-b border-gray-200">
+                <form action="{{ route('entries.store') }}" method="post">
+                    @csrf
+                    <label class="block text-gray-700 text-sm font-bold mb-2" for="body">Today's entry</label>
+                    <input type="hidden" name="body" id="body">
+                    <trix-editor input="body"></trix-editor>
+
+                    <button
+                        class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline mt-3">
+                        Save
+                    </button>
+                </form>
             </div>
         </div>
     </div>
-</x-app-layout>
+</div>
+
+@endsection
--- resources/views/entries/index.blade.php
+++ resources/views/entries/index.blade.php
@@ -1,24 +1,22 @@
-<x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Your entries') }}
-        </h2>
-    </x-slot>
+@extends('layouts.app')
 
-    @foreach($entries as $entry)
-        <div class="py-12">
-            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                    <div class="p-6 bg-white border-b border-gray-200">
-                        <p>{{ $entry->created_at->format('d M, Y') }}</p>
-                        <div class="my-4 italic text-2xl">{{ Str::limit($entry->body, 100) }}</div>
-                        <a href="{{ route('entries.show', $entry) }}"
-                            class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-                            Read more
-                        </a>
-                    </div>
+@section('content')
+
+@foreach($entries as $entry)
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 bg-white border-b border-gray-200">
+                    <p>{{ $entry->created_at->format('d M, Y') }}</p>
+                    <div class="my-4 italic text-2xl">{{ strip_tags(Str::limit($entry->body, 100)) }}</div>
+                    <a href="{{ route('entries.show', $entry) }}"
+                        class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                        Read more
+                    </a>
                 </div>
             </div>
         </div>
-    @endforeach
-</x-app-layout>
+    </div>
+@endforeach
+
+@endsection
